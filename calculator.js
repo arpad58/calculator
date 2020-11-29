498dcd07b78eeada51a35be87068e8a43a7a2f1d
Arpad <solar@hu.inter.net>
2020-11-29 15:15:16 +0100

    The diplay prints the characters of the buttons

---
+++ calculator.js
@@ -0,0 +1,90 @@
+document.querySelector(".btn-plus").addEventListener("click", function () {
+    const btnPlus = '+';
+    display.value = btnPlus;
+});
+
+document.querySelector(".btn-minus").addEventListener("click", function () {
+    const btnMinus = '-';
+    display.value = btnMinus;
+});
+
+document.querySelector(".btn-multiply").addEventListener("click", function () {
+    const btnMultiply = 'x';
+    display.value = btnMultiply;
+});
+
+document.querySelector(".btn-divide").addEventListener("click", function () {
+    const btnDivide = '/';
+    display.value = btnDivide;
+});
+
+
+
+
+document.querySelector(".btn-0").addEventListener("click", function () {
+    const btn0Value = 0;
+    display.value = btn0Value;
+});
+
+document.querySelector(".btn-1").addEventListener("click", function () {
+    const btn1Value = 1;
+    display.value = btn1Value;
+});
+
+document.querySelector(".btn-2").addEventListener("click", function () {
+    const btn2Value = 2;
+    display.value = btn2Value;
+});
+
+document.querySelector(".btn-3").addEventListener("click", function () {
+    const btn3Value = 3;
+    display.value = btn3Value;
+});
+
+document.querySelector(".btn-4").addEventListener("click", function () {
+    const btn4Value = 4;
+    display.value = btn4Value;
+});
+
+document.querySelector(".btn-5").addEventListener("click", function () {
+    const btn5Value = 5;
+    display.value = btn5Value;
+});
+
+document.querySelector(".btn-6").addEventListener("click", function () {
+    const btn6Value = 6;
+    display.value = btn6Value;
+});
+
+document.querySelector(".btn-7").addEventListener("click", function () {
+    const btn7Value = 7;
+    display.value = btn7Value;
+});
+
+document.querySelector(".btn-8").addEventListener("click", function () {
+    const btn8Value = 8;
+    display.value = btn8Value;
+});
+
+document.querySelector(".btn-9").addEventListener("click", function () {
+    const btn9Value = 9;
+    display.value = btn9Value;
+});
+
+
+
+
+document.querySelector(".btn-dot").addEventListener("click", function () {
+    const btnDotValue = '.';
+    display.value = btnDotValue;
+});
+
+document.querySelector(".btn-cancel").addEventListener("click", function () {
+    const btnCancelValue = '';
+    display.value = btnCancelValue;
+});
+
+//document.querySelector(".btn-equal").addEventListener("click", function () {
+//    const btnEqualValue = '=';
+//    display.value = btnEqualValue;
+//});
