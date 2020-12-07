cf35e20c365839eb9ddc0ea73003c29ec4803930
Arpad <solar@hu.inter.net>
2020-12-07 01:31:17 +0100

    html  js modified

--- calculator.js
+++ calculator.js
@@ -1,90 +1,52 @@
-document.querySelector(".btn-plus").addEventListener("click", function () {
-    const btnPlus = '+';
-    display.value = btnPlus;
-});
+'use strict';
 
-document.querySelector(".btn-minus").addEventListener("click", function () {
-    const btnMinus = '-';
-    display.value = btnMinus;
-});
+const calcs = {
+    '+': function (a, b) { return a + b },
+    '-': function (a, b) { return a - b },
+    '*': function (a, b) { return a * b },
+    '/': function (a, b) { return a / b },
+}
 
-document.querySelector(".btn-multiply").addEventListener("click", function () {
-    const btnMultiply = 'x';
-    display.value = btnMultiply;
-});
+const signs = [];
 
-document.querySelector(".btn-divide").addEventListener("click", function () {
-    const btnDivide = '/';
-    display.value = btnDivide;
-});
+for (let i = 0; i < signs.length; i += 1) {
+    console.log(calcs[signs[i]](10, 20));
+}
 
 
 
+let calcString = '';
+function handleClick() {
 
-document.querySelector(".btn-0").addEventListener("click", function () {
-    const btn0Value = 0;
-    display.value = btn0Value;
-});
+    const keys = document.querySelectorAll('.calc__num');
+    keys.forEach(key => {
+        document.addEventListener('click', () => {
 
-document.querySelector(".btn-1").addEventListener("click", function () {
-    const btn1Value = 1;
-    display.value = btn1Value;
-});
-
-document.querySelector(".btn-2").addEventListener("click", function () {
-    const btn2Value = 2;
-    display.value = btn2Value;
-});
-
-document.querySelector(".btn-3").addEventListener("click", function () {
-    const btn3Value = 3;
-    display.value = btn3Value;
-});
-
-document.querySelector(".btn-4").addEventListener("click", function () {
-    const btn4Value = 4;
-    display.value = btn4Value;
-});
-
-document.querySelector(".btn-5").addEventListener("click", function () {
-    const btn5Value = 5;
-    display.value = btn5Value;
-});
-
-document.querySelector(".btn-6").addEventListener("click", function () {
-    const btn6Value = 6;
-    display.value = btn6Value;
-});
-
-document.querySelector(".btn-7").addEventListener("click", function () {
-    const btn7Value = 7;
-    display.value = btn7Value;
-});
-
-document.querySelector(".btn-8").addEventListener("click", function () {
-    const btn8Value = 8;
-    display.value = btn8Value;
-});
-
-document.querySelector(".btn-9").addEventListener("click", function () {
-    const btn9Value = 9;
-    display.value = btn9Value;
-});
+            calcString += event.currentTarget.dataset.value;
+            writeOutput(calcString);
+        })
+    })
+}
 
 
+function writeOutput(value) {
+    document.querySelector('.output').value = value;
+}
 
+const acceptablesigns = ['+', '-', '*', '/']
+const nums = [];
 
-document.querySelector(".btn-dot").addEventListener("click", function () {
-    const btnDotValue = '.';
-    display.value = btnDotValue;
-});
-
-document.querySelector(".btn-cancel").addEventListener("click", function () {
-    const btnCancelValue = '';
-    display.value = btnCancelValue;
-});
-
-//document.querySelector(".btn-equal").addEventListener("click", function () {
-//    const btnEqualValue = '=';
-//    display.value = btnEqualValue;
-//});
+let num = '';
+for (let i = 0; i < calcString.length; i += 1) {
+    if (acceptablesigns.indexOf(calcString[i]) === -1) {
+        num += calcString[i];
+    }
+    else {
+        console.log(num);
+        nums.push(parseFloat(num));
+        num = '';
+        signs.push(calcString[i])
+    }
+}
+nums.push(parseFloat(num));
+console.log(nums, signs);
